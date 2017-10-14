0edc7f6529c9b916b63bac1fc3c5b12a679dbb9d
Acaido <trollral@gmail.com>
2017-10-14 13:04:40 +1000

    Работа со строками

---
+++ strings.py
@@ -0,0 +1,50 @@
+"""
+Вариант 1, строки (7).
+Определим слова в хэштеге. Дана строка, начинается с # и содержит набор слов,
+соединенных в одну строку без пробелов. Срока описана в стиле camelCase,
+то есть первое слово начинается с прописной буквы, а каждое следующее с заглавной.
+Например: #приветКакДела, #меняЗовутЕгорМнеМногоЛет и тд.
+Необходимо посчитать количество слов в строке и вывести количество этих слов.
+"""
+
+def parse_hash_str(msg: str) -> int:
+    if not msg.startswith('#'):
+        raise ValueError("It's not a hash string")
+    cnt = 0
+    msg = msg.replace('#', '', 1)
+    for i, char in enumerate(msg):
+        if char.islower() or not char.isalpha():
+            if not i == len(msg) - 1:
+                continue
+        cnt += 1
+    return cnt
+
+
+# def parse_hash_str(msg: str) -> list:
+#     if not msg.startswith('#'):
+#         raise ValueError("It's not a hash string")
+#     words = []
+#     word = []
+#     msg = msg.replace('#', '', 1)
+#     for i, char in enumerate(msg):
+#         if char.islower() or not char.isalpha():
+#             word.append(char)
+#             if not i == len(msg) - 1:
+#                 continue
+#         words.append(''.join(word))
+#         word[:] = []
+#         word.append(char)
+#     return words
+
+
+if __name__ == '__main__':
+    # msgs = [
+    #     '##1justAString',
+    #     '#приветКакДела',
+    #     '#меняЗовутЕгорМнеМногоЛет',
+    #     'blahBlah'
+    # ]
+    # for m in msgs:
+    #     print(m, parse_hash_str(m), sep='\t')
+    m = input()
+    print(parse_hash_str(m))
