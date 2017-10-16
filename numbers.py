82ae45d4c15bccfaca2cb9a944af4f0257d6db1d
acaido <trollral@gmail.com>
2017-10-16 21:46:36 +1000

    Убран закомменченный код

--- numbers.py
+++ numbers.py
@@ -6,7 +6,6 @@
 которое определяется r - длинной вектора z,
 и phi - углом поворота вектора Z против часовой стрелки.
 """
-# import math
 import cmath
 
 
@@ -16,14 +15,9 @@ def c_rect2pol(cnum: complex) -> tuple:
     :return: tuple consists of absolute value and argument of complex number
     in radians
     """
-    # return (math.sqrt(cnum.real ** 2 + cnum.imag ** 2),
-    #         math.atan2(cnum.imag, cnum.real))
-
     return cmath.polar(cnum)
 
 
 if __name__ == '__main__':
-    # z = 10 + 3j
-    # print(c_rect2pol(z))  # (10.44030650891055, 0.2914567944778671)
     z = input()
     print(c_rect2pol(complex(z.replace(' ', ''))))
