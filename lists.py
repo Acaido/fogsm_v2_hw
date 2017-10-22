ae729384a3e4c221811e714ff1490510e04ec7f8
acaido <trollral@gmail.com>
2017-10-22 16:50:33 +1000

    Работа со списками.

--- lists.py
+++ lists.py
@@ -2,15 +2,40 @@
 Вариант 7, списки 1.
 Дан список чисел. Выведите все элементы списка, которые больше предыдущего элемента.
 """
+import timeit
 
 
+def clock(func):
+    def wrapper(*args, **kwargs):
+        start = timeit.default_timer()
+        res = func(*args, **kwargs)
+        stop = timeit.default_timer()
+        return res, stop - start
+
+    return wrapper
+
+
+@clock
 def print_gt_prev(lst):
-    # todo: два вложенных списка не надо, можно сделать проще.
     for i, item in enumerate(lst):
-        print([x for x in lst if i > 0 and x > lst[i - 1]])
+        if i == 0:
+            continue
+        print([x for x in lst if x > lst[i - 1]])
+
+
+@clock
+def print_gt_prev_1(lst):
+    s = sorted(lst)
+    for item in lst[:-1]:
+        i = s.index(item)
+        print(s[i+1:])
 
 
 if __name__ == '__main__':
-    lst_in = list(map(float, input().split()))
-    print_gt_prev(lst_in)
-
+    values = list(map(float, input().split()))
+    print('values: ', values)
+    _, time = print_gt_prev(values)
+    print(time)
+    _, time_1 = print_gt_prev_1(values)
+    print(time_1)
+    print('time > time_1', time > time_1, abs(time-time_1))
