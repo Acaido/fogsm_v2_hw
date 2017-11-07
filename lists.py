cc4d0321ef09b2a640644cb6cf1b9cf30cc9d4a8
Acaido <trollral@gmail.com>
2017-11-07 23:20:46 +1000

    Изменил поведение принтилки в соотвествии с заданием.

--- lists.py
+++ lists.py
@@ -1,6 +1,7 @@
 """
 Вариант 7, списки 1.
-Дан список чисел. Выведите все элементы списка, которые больше предыдущего элемента.
+Дан список чисел. Выведите все элементы списка,
+которые больше предыдущего элемента.
 """
 import timeit
 
@@ -11,31 +12,21 @@ def clock(func):
         res = func(*args, **kwargs)
         stop = timeit.default_timer()
         return res, stop - start
-
     return wrapper
 
 
 @clock
-def print_gt_prev(lst):
-    for i, item in enumerate(lst):
-        if i == 0:
-            continue
-        print([x for x in lst if x > lst[i - 1]])
-
-
-@clock
-def print_gt_prev_1(lst):
-    s = sorted(lst)
-    for item in lst[:-1]:
-        i = s.index(item)
-        print(s[i+1:])
+def print_if_gt_prev(lst):
+    # Проверять нулевой элемент нет смысла. Делаем сдвиг и итерируем оба списка
+    # со сдвигом и без одновременно. Можно использовать индексы, но на больших
+    # списках работать они будут медленнее.
+    to_check = lst[1:]
+    for prev, cur in zip(lst, to_check):
+        if cur > prev:
+            print(cur, end=' ')
+    print('')
 
 
 if __name__ == '__main__':
     values = list(map(float, input().split()))
-    print('values: ', values)
-    _, time = print_gt_prev(values)
-    print(time)
-    _, time_1 = print_gt_prev_1(values)
-    print(time_1)
-    print('time > time_1', time > time_1, abs(time-time_1))
+    print_if_gt_prev(values)
